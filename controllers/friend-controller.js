5aa3613b17999afdc8477e25c50fb7d9792253eb
Sczygelski <avamariescz@gmail.com>
2022-10-21 10:53:17 -0500

    delete and update friend controllers

--- controllers/friend-controller.js
+++ controllers/friend-controller.js
@@ -43,7 +43,24 @@ const friendController = {
     },
     createUser({body},res){
         Friend.create(body)
-        .then(dbFriendData=> res.json(dbFriendData))
-        .catch((err)=> res.json(err))
-    }
+            .then(dbFriendData=> res.json(dbFriendData))
+            .catch((err)=> res.json(err))
+    },
+    updateUser({params,body},res){
+        Friend.findOneAndUpdate({_id: params.id}, body, {new:true, runVaidators:true})
+            .then((dbFriendData)=>{
+                if(!dbFriendData){
+                    res.status(404).json({message:'No users have this id'})
+                    return
+                }    
+                res.json(dbFriendData)
+            })
+            .catch(err=>res.json(err))
+    },
+    deleteUser({params}, res) {
+        Friend.findOneAndDelete({_id:params.id})
+            .then(dbFriendData=>res.json(dbFriendData))
+            .catch(err=> res.json(err))
+    }        
 }
+module.exports=friendController
