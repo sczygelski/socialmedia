c2cd1a7a060b1bd7cbcb7b3f6393d145adf95e7a
Sczygelski <avamariescz@gmail.com>
2022-10-21 12:08:36 -0500

    reaction controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,3 +1,5 @@
+const { param } = require("../routes")
+
 const {Thought,Friend}=reqwuire('../models')
 
 const thoughtController = {
@@ -44,5 +46,62 @@ const thoughtController = {
                 res.json(dbFriendData)
             })
             .catch((err)=>res.json(err))
+    },
+    removeThought({params},res){
+        Thought.findOneAndDelete({_id: params.thoughtId})
+            .then((deletedThought)=>{
+                if(!deletedThought){
+                    return res.status(404).json({message:'No thoughts have this id.'})
+                }
+                return Friend.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { thoughts: params.commentId } },
+                    { new: true }
+                )
+            })
+            .then((dbFriendData)=>{
+                if(!dbFriendData){
+                    res.status(404).json({message: 'No users have this id.'})
+                    return
+                }
+                res.json(dbFriendData)
+            })
+            .catch((err)=> res.json(err))
+    },
+    addReaction(req,res){
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { new: true, runValidators: true }
+        )
+        .then((dbThoughtData)=>{
+            if(!dbThoughtData){
+                res.status(404).json({message:'No thoughts have this id.'})
+                return
+            }
+            res.json(dbThoughtData)
+        })
+        .catch((err)=>{
+            console.log(err)
+            res.status(400).json(err)
+        })
+    },
+    deleteReaction(req,res){
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { new: true, runValidators: true }
+        )
+        .then((dbThoughtData)=>{
+            if(!dbThoughtData){
+                res.status(404).json({message:'No thoughts have this id.'})
+                return
+            }
+            res.json(dbThoughtData)
+        })
+        .catch((err)=>{
+            console.log(err)
+            res.status(400).json(err)
+        })
     }
 }
