9c0d26d09fbb186ce4170e0a8d04363b9f8b977e
Sczygelski <avamariescz@gmail.com>
2022-10-21 10:45:19 -0500

    create and find friend models

--- controllers/friend-controller.js
+++ controllers/friend-controller.js
@@ -2,7 +2,7 @@ const {Thought,Friend} = require('../models')
 
 const friendController = {
     getallUsers(req,res) {
-        username.find({})
+        Friend.find({})
             .populate({
                 path: 'thoughts',
                 select: '-__v'
@@ -18,5 +18,32 @@ const friendController = {
                 console.log(err)
                 res.sendStatus(400)
             })
+    },
+    getoneUser({params}, res) {
+        Friend.findOne({_id: params.id})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then((dbFriendData) => {
+                if(!dbFriendData){
+                    res.status(404).json({message: 'No users have this id.'})
+                }
+                res.json(dbFriendData)
+            })
+            .catch((err)=>{
+                console.log(err)
+                res.status(400).json(err)
+            })
+    },
+    createUser({body},res){
+        Friend.create(body)
+        .then(dbFriendData=> res.json(dbFriendData))
+        .catch((err)=> res.json(err))
     }
 }
